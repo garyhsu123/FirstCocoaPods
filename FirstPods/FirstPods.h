0438c8fd3c9c87a92ec340195b51c93685cf6fd0
Garry Hsu <garryhsu@richlink.ca>
2023-08-23 10:49:10 -0700

    first commit

---
+++ FirstPods/FirstPods.h
@@ -0,0 +1,18 @@
+//
+//  FirstPods.h
+//  FirstPods
+//
+//  Created by Garry Hsu on 2023-08-23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstPods.
+FOUNDATION_EXPORT double FirstPodsVersionNumber;
+
+//! Project version string for FirstPods.
+FOUNDATION_EXPORT const unsigned char FirstPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstPods/PublicHeader.h>
+
+
